6ab022d8b8c58ba415a6374fcccda7aa5f717637
acutiepieg <aryahigup@gmail.com>
2020-11-23 16:58:45 -0800

    started

--- src/_01_Intro_To_Sockets/client/ClientGreeter.java
+++ src/_01_Intro_To_Sockets/client/ClientGreeter.java
@@ -8,9 +8,9 @@ public class ClientGreeter {
    public static void main(String [] args) {
 	  //1. Create a String for the ip address of the server. 
 	  // If you don't know how to find a computer's ip address, ask about ifconfig on linux/mac and ipconfig on windows.
-      
+      String ipAddress = "192.168.4.54";
       //2. Create an integer for the server's port number
-      
+      int portNum;
       //3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
     
     	 //4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number
