13c36392493b7dd4278e0dcb1e4f41de00a3288a
acutiepieg <aryahigup@gmail.com>
2020-12-14 16:47:49 -0800

    started - work on Server next

--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -1,9 +1,51 @@
 package _02_Chat_Application;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import _00_Click_Chat.networking.Client;
+import _00_Click_Chat.networking.Server;
+
 /*
  * Using the Click_Chat example, write an application that allows a server computer to chat with a client computer.
  */
 
-public class ChatApp {
+public class ChatApp extends JFrame {
+	Server server;
+	Client client;
 
+	public static void main(String[] args) {
+		new ChatApp();
+	}
+
+	public ChatApp() {
+		int response = JOptionPane.showConfirmDialog(null, "Would you like to host a connection?", "Buttons!",
+				JOptionPane.YES_NO_OPTION);
+		if (response == JOptionPane.YES_OPTION) {
+			server = new Server(8080);
+			setTitle("Server");
+			System.out.println("Server started at: " + server.getIPAddress() + "\nPort: " + server.getPort());
+			server.start();
+		}
+		else {
+			setTitle("Client");
+			String ipString = null;
+			try {
+				ipString = (InetAddress.getLocalHost().getHostAddress());
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			String portString = JOptionPane.showInputDialog("Enter Port Number");
+			int portNum = Integer.parseInt(portString);
+			client = new Client(ipString, portNum);	
+			this.client.start();
+		}
+		setVisible(true);
+		setSize(500,400);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
--- src/_02_Chat_Application/Client.java
+++ src/_02_Chat_Application/Client.java
@@ -1,5 +1,50 @@
 package _02_Chat_Application;
 
-public class Client {
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+import javax.swing.ScrollPaneConstants;
+
+public class Client {
+	private String ip;
+	private int port;
+	
+	Socket connection;
+	
+	ObjectOutputStream os;
+	ObjectInputStream is;
+	
+	
+	
+	public Client (String ip, int port) {
+		this.ip = ip;
+		this.port = port;
+	}
+	
+	public void start() {
+		try {
+			connection = new Socket(ip, port);
+			os = new ObjectOutputStream(connection.getOutputStream());
+			is = new ObjectInputStream(connection.getInputStream());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		while(connection.isConnected()) {
+			try {
+				JTextPane pane = new JTextPane();
+				JScrollPane scroll = new JScrollPane(pane, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+						ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED );
+			   
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
 }
